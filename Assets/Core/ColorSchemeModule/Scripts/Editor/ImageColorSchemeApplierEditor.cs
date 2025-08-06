8b9f3c562f7746cb480122d6f335b3cb14cbc08e
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-06 16:28:27 +0300

    Added color appliers editors

---
+++ Assets/Core/ColorSchemeModule/Scripts/Editor/ImageColorSchemeApplierEditor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Core.ColorSchemeModule.Scripts.Editor
+{
+    [CustomEditor(typeof(ImageColorSchemeApplier))]
+    public class ImageColorSchemeApplierEditor : BaseColorSchemeApplierEditor { }
+}
---
+++ Assets/Core/ColorSchemeModule/Scripts/Editor/TextColorSchemeApplierEditor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Core.ColorSchemeModule.Scripts.Editor
+{
+    [CustomEditor(typeof(TextColorSchemeApplier))]
+    public class TextColorSchemeApplierEditor : BaseColorSchemeApplierEditor { }
+}
