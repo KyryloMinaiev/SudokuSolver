9c6c2ab564aaa8767b2095918c511226ded7612d
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-08-06 15:18:36 +0300

    Added basic color scheme types

---
+++ Assets/Core/ColorSchemeModule/Scripts/ColorRoleSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.ColorSchemeModule.Scripts
+{
+    [Serializable]
+    public struct ColorRoleSettings
+    {
+        public int ColorRoleID;
+        public Color Color;
+        
+        public ColorRoleSettings(int colorRoleID, Color color)
+        {
+            ColorRoleID = colorRoleID;
+            Color = color;
+        }
+    }
+}
---
+++ Assets/Core/ColorSchemeModule/Scripts/ColorSchemeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.ColorSchemeModule.Scripts
+{
+    [CreateAssetMenu(fileName = "ColorSchemeConfiguration", menuName = "Sudoku/ColorSchemeConfiguration", order = 0)]
+    public class ColorSchemeConfiguration : ScriptableObject
+    {
+        [SerializeField] private List<ColorRole> _colorRoles;
+        [SerializeField] private List<ColorScheme> _colorSchemes;
+        
+        public List<ColorRole> ColorRoles => _colorRoles;
+        public List<ColorScheme> ColorSchemes => _colorSchemes;
+
+        private void OnEnable()
+        {
+            foreach (ColorScheme colorScheme in _colorSchemes)
+            {
+                colorScheme.Initialize();
+            }
+        }
+    }
+}
